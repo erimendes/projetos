12bbcdd54f8c5886834f4531c6682cc98fb2a2bd
erimendes <eri.mendes32@gmail.com>
2025-01-31 16:59:31 -0300

    atualizaçao

--- galeria/galeria/admin.py
+++ galeria/galeria/admin.py
@@ -4,13 +4,18 @@ from galeria.models import Fotografia
 # Definindo uma classe com um nome mais convencional para administração
 class FotografiaAdmin(admin.ModelAdmin):
     # Campos que serão exibidos na lista de objetos
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada")
     
     # Campos que serão clicáveis para editar a fotografia
     list_display_links = ("id", "nome", "legenda")
     
     # Habilitando a busca pelo nome da fotografia
     search_fields = ("nome",)
+
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+
+    list_per_page = 1
 
 # Registrando o modelo 'Fotografia' com a configuração personalizada
 admin.site.register(Fotografia, FotografiaAdmin)
