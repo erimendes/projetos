80d6ab0eff93c8d26735dd5d82e31fac3dbdddbb
erimendes <eri.mendes32@gmail.com>
2025-01-31 16:59:31 -0300

    teste

--- galeria/galeria/admin.py
+++ galeria/galeria/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+# Definindo uma classe com um nome mais convencional para administração
+class FotografiaAdmin(admin.ModelAdmin):
+    # Campos que serão exibidos na lista de objetos
+    list_display = ("id", "nome", "legenda")
+    
+    # Campos que serão clicáveis para editar a fotografia
+    list_display_links = ("id", "nome", "legenda")
+    
+    # Habilitando a busca pelo nome da fotografia
+    search_fields = ("nome",)
+
+# Registrando o modelo 'Fotografia' com a configuração personalizada
+admin.site.register(Fotografia, FotografiaAdmin)
